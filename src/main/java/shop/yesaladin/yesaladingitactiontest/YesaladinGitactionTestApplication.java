c1958fbf489269570cb8cc9e336b8965ba9b4957
SuJeong68 <tidh1640@naver.com>
2023-01-02 21:05:09 +0900

    Initial Commit

---
+++ src/main/java/shop/yesaladin/yesaladingitactiontest/YesaladinGitactionTestApplication.java
@@ -0,0 +1,13 @@
+package shop.yesaladin.yesaladingitactiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesaladinGitactionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesaladinGitactionTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/yesaladin/yesaladingitactiontest/YesaladinGitactionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.yesaladin.yesaladingitactiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesaladinGitactionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

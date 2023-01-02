31b340c09861c573d00be011bb8a99490836e0b9
SuJeong68 <tidh1640@naver.com>
2023-01-02 21:05:51 +0900

    Initial Commit

--- src/main/java/shop/yesaladin/yesaladingitactiontest/YesaladinGitactionTestApplication.java
+++ src/main/java/shop/yesaladin/yesaladingitactiontest/YesaladinGitactionTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YesaladinGitactionTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YesaladinGitactionTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YesaladinGitactionTestApplication.class, args);
+    }
 
 }
